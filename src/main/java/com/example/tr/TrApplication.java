1e5e2e5339971d4429a90f59b99cc72558497591
yasser9947 <yasser9946.1@gmail.com>
2025-01-13 20:33:34 +0300

    first commit

---
+++ src/main/java/com/example/tr/TrApplication.java
@@ -0,0 +1,13 @@
+package com.example.tr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrApplication.class, args);
+    }
+
+}
